a09d568d19658c8dda12a275b9710aca9e5aded7
Gustavo Receputi <155767351+DevSharkMT@users.noreply.github.com>
2024-04-09 21:43:39 -0300

    Delete sistema_escolar directory

--- sistema_escolar/cookie.php
+++
@@ -1,7 +0,0 @@
-<?php
-    @$usuario_cookie = $_COOKIE["usuario"];
-    if(isset($usuario_cookie)){
-        echo"Bem-Vindo, $usuario_cookie <br>";
-        echo"<br>Essas são as opções <font color=\"BLUE\">DISPONÍVEIS</font> para você";
-    }
-?>
